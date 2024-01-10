72c6f4b0c37b5d30f306dcd9e669f16814129f78
oluwapelumi eyeladde <blueishincolour@gmail.com>
2024-01-10 03:25:37 +0100

    save

---
+++ lib/screens/auth/signup_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatefulWidget {
+  const SignupScreen({super.key});
+
+  @override
+  State<SignupScreen> createState() => SignupScreenState();
+}
+
+class SignupScreenState extends State<SignupScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            //logo
+            Icon(
+              Icons.key,
+              size: 100,
+              color: Colors.black,
+            ),
+            SizedBox(height: 15),
+            //email
+            TextField(),
+
+            SizedBox(height: 15),
+
+            //pasowed
+            TextField(),
+
+            SizedBox(height: 15),
+
+            //button
+            TextButton(
+                onPressed: () {},
+                child: Text(
+                  'login',
+                  style: TextStyle(
+                      color: Colors.blue, fontWeight: FontWeight.bold),
+                )),
+
+            SizedBox(height: 15),
+
+            //if no login. sign in
+            Row(
+              children: [
+                Text('you dont hava an account yet?  '),
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'signup',
+                      style: TextStyle(
+                          color: Colors.blue, fontWeight: FontWeight.bold),
+                    ))
+              ],
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+}
